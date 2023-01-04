cd9d2906b6af4fa2396b6622ec6913220f0c4883
Joshua Oh <joshua.jy.oh@gmail.com>
2023-01-04 12:39:09 -0500

    push

---
+++ app.js
@@ -0,0 +1,21 @@
+import * as Env from 'dotenv';
+import express from 'express';
+import * as Path from 'path';
+
+Env.config();
+
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use((req, res) => {
+    // res.setHeader('Access-Control-Allow-Origin', '*')
+    res.sendFile(Path.resolve('./index.html'));
+});
+
+app.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+});
