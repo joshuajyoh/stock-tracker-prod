6712467e263236732b6ef874c3f76b41e2f56b1b
Joshua Oh <joshua.jy.oh@gmail.com>
2023-01-30 16:24:45 -0500

    add https

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 import * as Env from 'dotenv';
 import express from 'express';
+import * as FileSystem from 'fs';
+import * as HTTP from 'http';
+import * as HTTPS from 'https';
 import * as Path from 'path';
 import StockDataRoutes from './src/routes/stock-data.js';
 
 Env.config();
 
-const host = process.env.HOST;
-const port = process.env.PORT;
-
 const app = express();
+
+// Certificate
+const privateKey = FileSystem.readFileSync('/etc/letsencrypt/live/joshuajyoh.com/privkey.pem', 'utf8');
+const certificate = FileSystem.readFileSync('/etc/letsencrypt/live/joshuajyoh.com/cert.pem', 'utf8');
+const ca = FileSystem.readFileSync('/etc/letsencrypt/live/joshuajyoh.com/chain.pem', 'utf8');
+
+const creds = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 app.use(express.static('public'));
 
@@ -22,6 +33,13 @@ app.get('/stock-tracker', (_, res) => {
 
 StockDataRoutes.setup(app);
 
-app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}/`);
+const httpServer = HTTP.createServer(app);
+const httpsServer = HTTPS.createServer(creds, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
 });
